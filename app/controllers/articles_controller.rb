fed30a5784aa5fe360e6239856819b0ceca3f717
Dalila baba <babaameur.dalila@gmail.com>
2021-08-20 16:07:39 +0100

    adding the new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article 
+    end
+
+    def create
+        # render plain: params[:article]
+        @article =  Article.new(params.required(:article).permit(:title, :description))
+        # render plain: @article.inspect
+        @article.save
+        # redirect_to article_path(@article)#rails will automaticlly retreive the ID from the object article
+        redirect_to @article #will have the same effect that above
+    end
 end 
