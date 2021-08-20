d978500623f4f61530dade4d52fd87314fb6162a
Dalila baba <babaameur.dalila@gmail.com>
2021-08-20 14:07:03 +0100

    add Articles show functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
     def home #home action
     end
+
+    def about #home action
+    end
 end
---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 6, maximum: 100} #makes sure that a title is never empty or nil, adding lengh in order to force a min and max lengh
+    validates :description, presence: true, length: {minimum: 10, maximum: 300} #makes sure that a title is never empty or nil
+end    
