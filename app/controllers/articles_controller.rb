3dd6ed3de3811eab601d8ace3f05acc0ddd413f9
Dalila baba <babaameur.dalila@gmail.com>
2021-08-22 10:40:45 +0100

    added few features: edit, update, delete, and allso added the links to the index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,16 +11,37 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
-        # render plain: params[:article]
-        @article =  Article.new(params.required(:article).permit(:title, :description))
-        # render plain: @article.inspect
-        if @article.save
-        # redirect_to article_path(@article)#rails will automaticlly retreive the ID from the object article
-          flash[:notice] = "Article was created successfully."
-          redirect_to @article #will have the same effect that above
-        else 
+      # render plain: params[:article]
+       @article =  Article.new(params.required(:article).permit(:title, :description))
+      # render plain: @article.inspect
+      if @article.save
+      # redirect_to article_path(@article)#rails will automaticlly retreive the ID from the object article
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article #will have the same effect that above
+      else 
           render 'new'
-        end
+      end
+    end
+
+    def update
+    @article = Article.find(params[:id])
+    if @article.update(params.required(:article).permit(:title, :description))
+      # redirect_to article_path(@article)#rails will automaticlly retreive the ID from the object article
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @article #will have the same effect that above
+      else 
+          render 'edit'
+      end
+    
+    end
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to @article
     end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create] #the only statment is for specifing only one route
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update] #the only statment is for specifing only one route
+  resources :articles #this is showing all the liste provided by rails routes --expended
 end
