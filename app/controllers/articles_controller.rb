411d9af38934e65d6cc4f7164607984487877ed9
Dalila baba <babaameur.dalila@gmail.com>
2021-08-22 11:36:01 +0100

    new feature: partials, and used DRY method on Article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
     end 
 
     def index
@@ -12,12 +14,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
       # render plain: params[:article]
-       @article =  Article.new(params.required(:article).permit(:title, :description))
+       @article =  Article.new(article_params)
       # render plain: @article.inspect
       if @article.save
       # redirect_to article_path(@article)#rails will automaticlly retreive the ID from the object article
@@ -29,8 +30,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-    @article = Article.find(params[:id])
-    if @article.update(params.required(:article).permit(:title, :description))
+    if @article.update(article_params)
       # redirect_to article_path(@article)#rails will automaticlly retreive the ID from the object article
         flash[:notice] = "Article was updated successfully."
         redirect_to @article #will have the same effect that above
@@ -40,8 +40,17 @@ class ArticlesController < ApplicationController
     
     end
     def destroy
-      @article = Article.find(params[:id])
       @article.destroy
       redirect_to @article
+    end
+    
+    private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.required(:article).permit(:title, :description)
     end
 end 
