7e1c647e39205ad3de49407728dc18cce6ed4198
Dalila baba <babaameur.dalila@gmail.com>
2021-08-25 13:27:56 +0100

    made the article author automated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :articles
     validates :username, presence: true,
                          uniqueness: {case_sensitive: false}, 
-                         length: {minimum: 3, maximum: 5}
+                         length: {minimum: 3, maximum: 10}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                             
     validates :email, presence: true,
                       uniqueness: {case_sensitive: false}, 
