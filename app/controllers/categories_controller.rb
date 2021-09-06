73a3b7ddefe2c23a367acf87911073aea647ed43
Dalila <babaameur.dalila@gmail.com>
2021-09-06 10:32:31 +0100

    befor going back

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,16 @@
 class CategoriesController < ApplicationController
 
     def new
+      @category = Category.new
     end
 
     def show
     end
 
     def index
+    end
+
+    def create
+
     end
 end
